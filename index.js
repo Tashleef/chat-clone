d633ce32c8067e3108552617f6aad15be0313911
tashleef <alayoubifaek@gmail.com>
2023-04-15 05:33:20 +0300

    completing the basics requirenment for this app

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {formateMessage} = require("./structure/message");
+const { formateMessage } = require("./structure/message");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
@@ -8,11 +8,13 @@ app.use(express.static("public"));
 
 io.on("connection", (socket) => {
 	console.log("Connected");
+	socket.on("joinRoom", ({ username, room }) => {
+		socket.join(room);
 
-	socket.on("joinRoom", ({username, room}) => {
-		io.emit("roomUsers", {room, username});
+		io.to(room).emit("roomUsers", { room, username });
 		socket.on("chatMessage", (msg) => {
-			io.emit(
+			console.log(msg);
+			io.to(room).emit(
 				"message",
 				formateMessage({
 					username,
@@ -20,7 +22,7 @@ io.on("connection", (socket) => {
 				})
 			);
 		});
-		io.emit(
+		io.to(room).emit(
 			"message",
 			formateMessage({
 				username,
